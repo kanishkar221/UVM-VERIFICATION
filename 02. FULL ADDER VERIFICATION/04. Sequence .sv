09f712ba48aae3cbc73e8306af1d26493572b249
KANISHKA R <kanishka.r221@gmail.com>
2026-02-10 14:15:41 +0530

    Update 04. Sequence .sv

--- 02. FULL ADDER VERIFICATION/04. Sequence .sv
+++ 02. FULL ADDER VERIFICATION/04. Sequence .sv
@@ -1,1 +1,18 @@
-
+class FA_sequence extends uvm_sequence #(FA_seq_item);
+  
+  `uvm_object_utils(FA_sequence)
+  
+  function new(string name = "FA_sequence");
+    super.new(name);
+  endfunction
+  
+  task body();
+    FA_seq_item req;
+    `uvm_info("SEQ", "Sequence starts", UVM_LOW)
+    repeat(10)begin
+      req = FA_seq_item::type_id::create("req");
+      `uvm_do(req)
+    end
+  endtask
+  
+endclass      
