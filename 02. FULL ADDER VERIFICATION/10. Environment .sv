7922539299d0dbf5f983783859d82e44b92a4e19
KANISHKA R <kanishka.r221@gmail.com>
2026-02-10 14:18:51 +0530

    Update 10. Environment .sv

--- 02. FULL ADDER VERIFICATION/10. Environment .sv
+++ 02. FULL ADDER VERIFICATION/10. Environment .sv
@@ -1,1 +1,24 @@
-
+class FA_env extends uvm_env;
+  
+  `uvm_component_utils(FA_env)
+  
+  FA_agent agent;
+  FA_scoreboard sb;
+  
+  function new(string name = "FA_env", uvm_component parent);
+    super.new(name, parent);
+    `uvm_info("ENV", "Environment Created", UVM_LOW)
+  endfunction
+  
+  function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    agent = FA_agent:: type_id ::create("agent",this);
+    sb = FA_scoreboard::type_id:: create("sb",this);
+  endfunction
+  
+  function void  connect_phase (uvm_phase phase);
+    super.connect_phase(phase);
+    agent.monitor.mon_ap.connect(sb.sb_imp);
+  endfunction
+  
+endclass
