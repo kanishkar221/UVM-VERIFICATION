dff092ce0431b7d4a18190b3470ad9e65a4ce13e
KANISHKA R <kanishka.r221@gmail.com>
2026-02-10 09:36:27 +0530

    Update 04. Sequence .sv

--- 01. HALF ADDER VERIFICATION /04. Sequence .sv
+++ 01. HALF ADDER VERIFICATION /04. Sequence .sv
@@ -1,1 +1,18 @@
-
+class ha_sequence extends uvm_sequence #(ha_seq_item);
+  
+  `uvm_object_utils(ha_sequence)
+  
+  function new(string name = "ha_sequence");
+    super.new(name);
+  endfunction
+  
+  task body();
+    ha_seq_item req;
+    `uvm_info("SEQ", "Sequence starts", UVM_LOW)
+    repeat(10)begin
+      req = ha_seq_item::type_id::create("req");
+      `uvm_do(req)
+    end
+  endtask
+  
+endclass      
