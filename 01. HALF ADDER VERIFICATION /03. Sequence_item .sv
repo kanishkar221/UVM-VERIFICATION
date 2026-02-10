da8ffed321f7e20101295f55da64de91ba0c52a4
KANISHKA R <kanishka.r221@gmail.com>
2026-02-10 09:35:59 +0530

    Update 03. Sequence_item .sv

--- 01. HALF ADDER VERIFICATION /03. Sequence_item .sv
+++ 01. HALF ADDER VERIFICATION /03. Sequence_item .sv
@@ -1,1 +1,14 @@
-
+class ha_seq_item extends uvm_sequence_item;
+  
+  `uvm_object_utils(ha_seq_item)   
+  
+  rand bit a;
+  rand bit b;
+  bit sum;
+  bit carry;
+  
+  function new (string name = "ha_seq_item");
+    super.new(name);
+  endfunction
+  
+endclass
