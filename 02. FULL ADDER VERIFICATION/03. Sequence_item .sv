11e6de6b2ffbb0b91d2fe42e389a3ea0ac82e811
KANISHKA R <kanishka.r221@gmail.com>
2026-02-10 14:15:12 +0530

    Update 03. Sequence_item .sv

--- 02. FULL ADDER VERIFICATION/03. Sequence_item .sv
+++ 02. FULL ADDER VERIFICATION/03. Sequence_item .sv
@@ -1,1 +1,15 @@
-
+class FA_seq_item extends uvm_sequence_item;
+  
+  `uvm_object_utils(FA_seq_item)   
+  
+  rand bit a;
+  rand bit b;
+  rand bit cin;
+  bit sum;
+  bit cout;
+  
+  function new (string name = "FA_seq_item");
+    super.new(name);
+  endfunction
+  
+endclass
