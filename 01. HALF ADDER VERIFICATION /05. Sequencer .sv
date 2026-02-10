b2954f473f9fdadb501baf7566ac0e48ea35d255
KANISHKA R <kanishka.r221@gmail.com>
2026-02-10 09:37:01 +0530

    Update 05. Sequencer .sv

--- 01. HALF ADDER VERIFICATION /05. Sequencer .sv
+++ 01. HALF ADDER VERIFICATION /05. Sequencer .sv
@@ -1,1 +1,9 @@
-
+class ha_sequencer extends uvm_sequencer #(ha_seq_item);
+  
+  `uvm_component_utils(ha_sequencer)
+  
+  function new(string name = "ha_sequencer", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+endclass
