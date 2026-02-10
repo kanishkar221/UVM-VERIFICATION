02d3edc255e00d030d60c7fff6232dc4ecce5932
KANISHKA R <kanishka.r221@gmail.com>
2026-02-10 14:16:03 +0530

    Update 05. Sequencer .sv

--- 02. FULL ADDER VERIFICATION/05. Sequencer .sv
+++ 02. FULL ADDER VERIFICATION/05. Sequencer .sv
@@ -1,1 +1,9 @@
-
+class FA_sequencer extends uvm_sequencer #(FA_seq_item);
+  
+  `uvm_component_utils(FA_sequencer)
+  
+  function new(string name = "FA_sequencer", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+endclass
