90dc684d54b3f4abf85ba1dc1793a8f708226948
KANISHKA R <kanishka.r221@gmail.com>
2026-02-10 14:20:13 +0530

    Update 12. Testbench .sv

--- 02. FULL ADDER VERIFICATION/12. Testbench .sv
+++ 02. FULL ADDER VERIFICATION/12. Testbench .sv
@@ -1,1 +1,38 @@
+import uvm_pkg::*;          
+`include "uvm_macros.svh"
 
+`include "interface.sv"
+`include "FA_seq_item.sv"
+`include "FA_sequence.sv"
+`include "FA_sequencer.sv"
+`include "FA_driver.sv"
+`include "FA_monitor.sv"
+`include "FA_agent.sv"
+`include "FA_scoreboard.sv"
+`include "FA_env.sv"
+`include "FA_test.sv"
+
+module top;
+  
+  FA_if vif();
+  
+  FA dut(.a(vif.a),
+         .b(vif.b),
+         .cin(vif.cin),
+         .sum(vif.sum),
+         .cout(vif.cout)
+        );
+  
+  initial begin
+    uvm_config_db#(virtual FA_if)::set(null,"*","vif",vif);
+    run_test ("FA_test");
+  end
+  
+  initial begin
+    $dumpfile("FA_UVM.vcd");
+    $dumpvars;
+    #100;
+    $finish;
+  end
+  
+endmodule
