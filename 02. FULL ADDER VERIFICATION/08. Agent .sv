5ec4289b29e109d04316320267fac2dd83eeeb94
KANISHKA R <kanishka.r221@gmail.com>
2026-02-10 14:17:54 +0530

    Update 08. Agent .sv

--- 02. FULL ADDER VERIFICATION/08. Agent .sv
+++ 02. FULL ADDER VERIFICATION/08. Agent .sv
@@ -1,1 +1,25 @@
-
+class FA_agent extends uvm_agent;
+  
+  `uvm_component_utils(FA_agent)
+  
+  FA_driver driver;
+  FA_monitor monitor;
+  FA_sequencer sequencer;
+  
+  function new(string name = "FA_Agent", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+  function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    sequencer = FA_sequencer:: type_id :: create("sequencer", this); 
+    driver = FA_driver::type_id::create("driver",this);
+    monitor = FA_monitor:: type_id :: create("monitor",this);
+  endfunction
+  
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    driver.seq_item_port.connect(sequencer.seq_item_export);
+  endfunction
+  
+endclass
