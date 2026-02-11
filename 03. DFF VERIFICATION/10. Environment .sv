934728b8769321ccbac9b8efd173c6ae337b8dca
KANISHKA R <kanishka.r221@gmail.com>
2026-02-11 17:01:53 +0530

    Update 10. Environment .sv

--- 03. DFF VERIFICATION/10. Environment .sv
+++ 03. DFF VERIFICATION/10. Environment .sv
@@ -1,1 +1,25 @@
-
+class dff_env extends uvm_env;
+  
+  `uvm_component_utils(dff_env)
+  
+  dff_agent agent;
+  dff_scoreboard sb;
+  
+  function new(string name = "environment", uvm_component parent);
+    super.new(name, parent);
+    `uvm_info("ENV", "Environment Created", UVM_LOW)
+  endfunction
+  
+  function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    agent = dff_agent:: type_id ::create("agent",this);
+    sb = dff_scoreboard::type_id:: create("sb",this);
+  endfunction
+  
+  function void  connect_phase (uvm_phase phase);
+    super.connect_phase(phase);
+    agent.monitor.mon_ap.connect(sb.sb_imp);
+  endfunction
+  
+endclass
+   
