5fc1f546f00964b47f2ab5cd60af80c067ab42c3
KANISHKA R <kanishka.r221@gmail.com>
2026-02-11 18:27:17 +0530

    Update 05. Sequencer .sv

--- 04. MEMORY/05. Sequencer .sv
+++ 04. MEMORY/05. Sequencer .sv
@@ -1,1 +1,9 @@
-
+class ram_sequencer extends uvm_sequencer#(ram_seq_item);
+  
+  `uvm_component_utils(ram_sequencer)
+  
+  function new(string name = "ram_sequencer",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+endclass
