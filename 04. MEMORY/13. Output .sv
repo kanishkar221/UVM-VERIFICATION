8e3e8b016455379be44654ee45e005d12425ed3e
KANISHKA R <kanishka.r221@gmail.com>
2026-02-11 18:55:33 +0530

    Update 13. Output .sv

--- 04. MEMORY/13. Output .sv
+++ 04. MEMORY/13. Output .sv
@@ -10,3 +10,13 @@ UVM_INFO scoreboard.sv(29) @ 66: uvm_test_top.env.sb [SB] PASS ---> Input:clk=0 
 UVM_INFO scoreboard.sv(23) @ 76: uvm_test_top.env.sb [SB] WRITE PASS -----> Input:clk=0  wr=1  d_in=58 addr=6  d_out=0
 UVM_INFO scoreboard.sv(23) @ 86: uvm_test_top.env.sb [SB] WRITE PASS -----> Input:clk=0  wr=1  d_in=221 addr=2  d_out=0
 UVM_INFO scoreboard.sv(23) @ 96: uvm_test_top.env.sb [SB] WRITE PASS -----> Input:clk=0  wr=1  d_in=14 addr=8  d_out=0
+UVM_INFO scoreboard.sv(29) @ 106: uvm_test_top.env.sb [SB] PASS ---> Input:clk=0  wr=0  d_in=229  addr=9  Actual Output: d_out=155 || Expected Output: exp_d_out=155
+UVM_INFO scoreboard.sv(23) @ 116: uvm_test_top.env.sb [SB] WRITE PASS -----> Input:clk=0  wr=1  d_in=74 addr=8  d_out=155
+UVM_INFO scoreboard.sv(29) @ 126: uvm_test_top.env.sb [SB] PASS ---> Input:clk=0  wr=0  d_in=223  addr=7  Actual Output: d_out=0 || Expected Output: exp_d_out=0
+UVM_INFO scoreboard.sv(29) @ 136: uvm_test_top.env.sb [SB] PASS ---> Input:clk=0  wr=0  d_in=120  addr=4  Actual Output: d_out=251 || Expected Output: exp_d_out=251
+UVM_INFO scoreboard.sv(29) @ 146: uvm_test_top.env.sb [SB] PASS ---> Input:clk=0  wr=0  d_in=88  addr=2  Actual Output: d_out=221 || Expected Output: exp_d_out=221
+UVM_INFO scoreboard.sv(23) @ 156: uvm_test_top.env.sb [SB] WRITE PASS -----> Input:clk=0  wr=1  d_in=175 addr=4  d_out=221
+UVM_INFO scoreboard.sv(29) @ 166: uvm_test_top.env.sb [SB] PASS ---> Input:clk=0  wr=0  d_in=78  addr=10  Actual Output: d_out=0 || Expected Output: exp_d_out=0
+UVM_INFO scoreboard.sv(23) @ 176: uvm_test_top.env.sb [SB] WRITE PASS -----> Input:clk=0  wr=1  d_in=240 addr=0  d_out=0
+UVM_INFO scoreboard.sv(29) @ 186: uvm_test_top.env.sb [SB] PASS ---> Input:clk=0  wr=0  d_in=195  addr=5  Actual Output: d_out=188 || Expected Output: exp_d_out=188
+UVM_INFO scoreboard.sv(29) @ 196: uvm_test_top.env.sb [SB] PASS ---> Input:clk=0  wr=0  d_in=130  addr=2  Actual Output: d_out=221 || Expected Output: exp_d_out=221
