a2fac1cd34164f287733cde4629aec9573cf86fc
KANISHKA R <kanishka.r221@gmail.com>
2026-02-11 16:59:40 +0530

    Update 05. Sequencer .sv

--- 03. DFF VERIFICATION/05. Sequencer .sv
+++ 03. DFF VERIFICATION/05. Sequencer .sv
@@ -1,1 +1,10 @@
-
+class dff_sequencer extends uvm_sequencer#(dff_seq_item);
+  
+  `uvm_component_utils(dff_sequencer)
+  
+  function new(string name = "dff_sequencer",uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+endclass
+  
