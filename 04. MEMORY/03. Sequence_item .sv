87d9d854bc596d77aeca32afb2c0a4679fc92c84
KANISHKA R <kanishka.r221@gmail.com>
2026-02-11 18:16:04 +0530

    Update 03. Sequence_item .sv

--- 04. MEMORY/03. Sequence_item .sv
+++ 04. MEMORY/03. Sequence_item .sv
@@ -1,1 +1,18 @@
-
+class ram_seq_item extends uvm_sequence_item;
+  
+  `uvm_object_utils(ram_seq_item)
+  
+  bit clk;
+  rand bit wr;
+  rand bit [7:0] d_in;
+  rand bit [3:0] addr;
+  bit [7:0] d_out;
+  
+  function new (name = "ram_seq_item");
+    super.new(name);
+  endfunction
+  
+  constraint c1 {wr dist{0:=3, 1:=7};}
+  constraint c2 {addr inside {[0:10]};}
+  
+endclass
