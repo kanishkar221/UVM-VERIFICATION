586bbb6820189bf36cab8cdb847b071db0d82025
KANISHKA R <kanishka.r221@gmail.com>
2026-02-11 16:58:39 +0530

    Update 03. Sequence_item .sv

--- 03. DFF VERIFICATION/03. Sequence_item .sv
+++ 03. DFF VERIFICATION/03. Sequence_item .sv
@@ -1,1 +1,17 @@
-
+class dff_seq_item extends uvm_sequence_item;
+  
+  `uvm_object_utils(dff_seq_item)
+  
+  rand bit reset;
+  rand bit d;
+  bit q;
+  
+  function new(string name = "dff_seq_item");
+    super.new(name);
+  endfunction
+  
+  constraint c1 {reset dist{ 0:=5 , 1:=5};}
+  constraint c2 {d dist{ 0:=5 , 1:=5};}
+  
+endclass
+  
