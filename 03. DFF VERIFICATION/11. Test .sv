7e1f4ce76693bff0220834c8e18554a9da761d15
KANISHKA R <kanishka.r221@gmail.com>
2026-02-11 17:02:41 +0530

    Update 11. Test .sv

--- 03. DFF VERIFICATION/11. Test .sv
+++ 03. DFF VERIFICATION/11. Test .sv
@@ -1,1 +1,27 @@
+class dff_test extends uvm_test;
+  
+  `uvm_component_utils(dff_test)
+  
+  dff_env env;
+  dff_sequence seq;
 
+  function new(string name = "test", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = dff_env:: type_id::create("env",this);
+    seq = dff_sequence::type_id::create("seq");
+  endfunction
+  
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+    phase.raise_objection(this);
+    seq.start(env.agent.sequencer);
+    #200;
+    phase.drop_objection(this);
+  endtask
+   
+endclass
+   
