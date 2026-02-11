6e3826ed11c9b2c2bc430ac29b0cddb5b8a10acb
KANISHKA R <kanishka.r221@gmail.com>
2026-02-11 16:57:36 +0530

    Update 01. Design .sv

--- 03. DFF VERIFICATION/01. Design .sv
+++ 03. DFF VERIFICATION/01. Design .sv
@@ -1,1 +1,15 @@
-
+module dff(
+  input logic clk,
+  input logic reset,
+  input logic d,
+  output logic q
+);
+  
+  always_ff @ (posedge clk) begin
+    if(reset)
+      q <= 0;
+    else
+      q <= d;
+  end
+  
+endmodule    
