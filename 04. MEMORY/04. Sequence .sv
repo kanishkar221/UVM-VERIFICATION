b04ce2f4c2dca7b313d7cd17f75f6fb848c6348b
KANISHKA R <kanishka.r221@gmail.com>
2026-02-11 18:16:27 +0530

    Update 04. Sequence .sv

--- 04. MEMORY/04. Sequence .sv
+++ 04. MEMORY/04. Sequence .sv
@@ -1,1 +1,18 @@
-
+class ram_sequence extends uvm_sequence#(ram_seq_item);
+  
+  `uvm_object_utils(ram_sequence)
+  
+  function new(name = "ram_sequence");
+    super.new(name);
+  endfunction
+  
+  task body();
+    ram_seq_item req;
+    `uvm_info ("SEQ","Sequence Starts",UVM_LOW)
+    repeat(20)begin
+      req = ram_seq_item::type_id::create("req");
+      `uvm_do(req)
+    end
+  endtask
+  
+endclass
