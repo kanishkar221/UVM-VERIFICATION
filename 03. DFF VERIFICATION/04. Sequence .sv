43f983fc744744603496debc2cfccd5fba87116f
KANISHKA R <kanishka.r221@gmail.com>
2026-02-11 16:59:07 +0530

    Update 04. Sequence .sv

--- 03. DFF VERIFICATION/04. Sequence .sv
+++ 03. DFF VERIFICATION/04. Sequence .sv
@@ -1,1 +1,23 @@
-
+class dff_sequence extends uvm_sequence#(dff_seq_item);
+  
+  `uvm_object_utils(dff_sequence)
+  
+  function new(string name = "dff_sequence");
+    super.new(name);
+  endfunction
+  
+  task body();
+    dff_seq_item req;
+    `uvm_info("SEQ","Sequence starts",UVM_LOW)
+    
+    repeat(10) begin
+      req= dff_seq_item::type_id::create("req");
+      start_item(req);
+      if(!req.randomize()) 
+        `uvm_error("SEQ", "Randomize failed");
+      finish_item(req);
+    end
+  endtask
+  
+endclass 
+  
