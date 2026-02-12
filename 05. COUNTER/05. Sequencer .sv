9bcce825c4c4aaeaa529cb91db366e17d8b9b029
KANISHKA R <kanishka.r221@gmail.com>
2026-02-12 13:56:09 +0530

    Update 05. Sequencer .sv

--- 05. COUNTER/05. Sequencer .sv
+++ 05. COUNTER/05. Sequencer .sv
@@ -1,1 +1,10 @@
-
+class counter_sequencer extends uvm_sequencer #(counter_seq_item);
+  
+  `uvm_component_utils(counter_sequencer)
+  
+  function new(string name = "counter_sequencer", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+endclass
+  
