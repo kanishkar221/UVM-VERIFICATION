e45a8045a1eaf617da7066dc270b2cea69751480
KANISHKA R <kanishka.r221@gmail.com>
2026-02-12 13:58:36 +0530

    Update 08. Agent .sv

--- 05. COUNTER/08. Agent .sv
+++ 05. COUNTER/08. Agent .sv
@@ -1,1 +1,25 @@
-
+class counter_agent extends uvm_agent;
+  
+  `uvm_component_utils(counter_agent)
+  
+  counter_driver driver;
+  counter_monitor monitor;
+  counter_sequencer sequencer;
+  
+  function new(string name = "counter_agent",uvm_component parent);
+    super.new(name, parent);
+  endfunction
+  
+  function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    sequencer = counter_sequencer::type_id::create("sequencer",this);
+    driver = counter_driver::type_id::create("driver",this);
+    monitor = counter_monitor::type_id::create("monitor",this);
+  endfunction
+  
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    driver.seq_item_port.connect(sequencer.seq_item_export);
+  endfunction
+  
+endclass
