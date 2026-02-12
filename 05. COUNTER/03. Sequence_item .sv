f117a1c7cb102c9feae33cee2aead045e63a1dac
KANISHKA R <kanishka.r221@gmail.com>
2026-02-12 13:55:00 +0530

    Update 03. Sequence_item .sv

--- 05. COUNTER/03. Sequence_item .sv
+++ 05. COUNTER/03. Sequence_item .sv
@@ -1,1 +1,14 @@
-
+class counter_seq_item extends uvm_sequence_item;
+  
+  `uvm_object_utils(counter_seq_item)
+  
+  bit reset;
+  rand bit enable;
+  bit [3:0] count;
+  
+  function new (string name = "counter_seq_item");
+    super.new(name);
+  endfunction
+  
+endclass
+ 
