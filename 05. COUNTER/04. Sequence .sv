ac3e471261be1678c165a9f1431d8d15e9e41739
KANISHKA R <kanishka.r221@gmail.com>
2026-02-12 13:55:48 +0530

    Update 04. Sequence .sv

--- 05. COUNTER/04. Sequence .sv
+++ 05. COUNTER/04. Sequence .sv
@@ -1,1 +1,19 @@
-
+class counter_sequence extends uvm_sequence #(counter_seq_item);
+  
+  `uvm_object_utils(counter_sequence)
+  
+  function new (string name = "counter_sequence");
+    super.new(name);
+  endfunction
+  
+  task body();
+    counter_seq_item req;
+    `uvm_info("SEQ", "Sequence starts",UVM_LOW)
+    repeat(10)begin
+      req = counter_seq_item::type_id::create("req");
+      `uvm_do(req)
+    end
+  endtask
+  
+endclass
+  
